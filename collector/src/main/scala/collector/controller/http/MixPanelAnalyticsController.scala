f4e65cbc44c017d1f025079c88c9f3fb67160e56
Tien Nguyen <tiennt4@rever.vn>
2017-01-04 16:40:14 +0700

    Update MixPannel api

--- collector/src/main/scala/collector/controller/http/MixPanelAnalyticsController.scala
+++ collector/src/main/scala/collector/controller/http/MixPanelAnalyticsController.scala
@@ -15,7 +15,7 @@ class MixPanelAnalyticsController  @Inject()(service: AnalyticsService) extends 
 get("/mixpanel/track") {
     request: Request => {
       service.process(
-        AnalyticRequest("people_analytic", 0,
+        AnalyticRequest(request.getParam("topic"), request.getIntParam("version", 0),
           new String(Base64.getDecoder.decode(request.params("data"))),
           request.params.getOrElse("_", System.currentTimeMillis().toString).toLong))
       response.ok
@@ -25,7 +25,7 @@ get("/mixpanel/track") {
   post("/mixpanel/track") {
     request: Request => {
       service.process(
-        AnalyticRequest("people_analytic", 0,
+        AnalyticRequest(request.getParam("topic"), request.getIntParam("version", 0),
           new String(Base64.getDecoder.decode(request.params("data"))),
           request.params.getOrElse("_", System.currentTimeMillis().toString).toLong))
       response.ok
