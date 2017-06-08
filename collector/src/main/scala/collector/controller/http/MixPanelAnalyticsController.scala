a847f194dcdc0c67cfb995408ee67b745a1a75b1
Tien Nguyen <tiennt4@rever.vn>
2017-06-08 14:17:35 +0700

    Response as Gif for api /mixpanel/track

--- collector/src/main/scala/collector/controller/http/MixPanelAnalyticsController.scala
+++ collector/src/main/scala/collector/controller/http/MixPanelAnalyticsController.scala
@@ -1,12 +1,16 @@
 package collector.controller.http
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
 import java.nio.charset.Charset
 import java.util.Base64
+import javax.imageio.ImageIO
 import javax.inject.Inject
 
 import collector.domain.AnalyticRequest
 import collector.service.AnalyticsService
-import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.{MediaType, Request}
 import com.twitter.finatra.http.Controller
 
 /**
@@ -14,12 +18,26 @@ import com.twitter.finatra.http.Controller
   */
 class MixPanelAnalyticsController @Inject()(service: AnalyticsService) extends Controller {
 
+  //Create gif
+  private final val imgInByte = {
+    val baos = new ByteArrayOutputStream()
+    val singlePixelImage = new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR)
+    val transparent = new Color(0, 0, 0, 0)
+    singlePixelImage.setRGB(0, 0, transparent.getRGB)
+    ImageIO.write(singlePixelImage, "gif", baos)
+    baos.flush()
+    val tmp = baos.toByteArray
+    baos.close()
+    tmp
+  }
+
+
   get("/mixpanel/track") {
     request: Request => {
       service.process(
         AnalyticRequest(request.getParam("topic"), request.getIntParam("version", 0),
           new String(Base64.getDecoder.decode(request.getParam("data")), Charset.forName("utf-8"))))
-      response.ok
+      response.ok.contentType(MediaType.Gif).body(imgInByte)
     }
   }
 
@@ -28,8 +46,7 @@ class MixPanelAnalyticsController @Inject()(service: AnalyticsService) extends C
       service.process(
         AnalyticRequest(request.getParam("topic"), request.getIntParam("version", 0),
           new String(Base64.getDecoder.decode(request.params("data")), Charset.forName("UTF-8"))))
-
-      response.ok
+      response.ok.contentType(MediaType.Gif).body(imgInByte)
     }
   }
 
